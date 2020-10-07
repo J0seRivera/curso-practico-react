dc91864b3f03535f507a1c8ae007e42d68bc5d57
Jose-Rivera <joseriveradl@gmail.com>
2020-10-06 23:44:46 -0600

    adding fake login and logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video.png';
 import userIcon from '../assets/static/user-icon.png';
 
+
 const Header = props => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({
+    })
+  }
+
   return (
     <header className="header">
       <Link to="/">
@@ -23,18 +32,32 @@ const Header = props => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li>
-            <a href="log-in.html">Cuenta</a>
-          </li>
-          <li>
-            <Link to="/login">
-              Iniciar Sesión
-          </Link>
-          </li>
+          {
+            hasUser ?
+              <li>
+                <a href="log-in.html">{user.name}</a>
+              </li>
+              : null
+          }
+
+          {
+            hasUser ?
+              <li><a href="#logout" onClick={handleLogout}>Cerrar Sesión</a> </li>
+              :
+              <li>
+                <Link to="/login">
+                  Iniciar Sesión
+                </Link>
+              </li>
+          }
         </ul>
       </div>
     </header>
   );
+}
+
+Header.propTypes = {
+  user: PropTypes.object
 }
 
 const mapStateToProps = state => {
@@ -43,4 +66,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,10 +4,8 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 
 import '../assets/styles/App.scss';
-
 
 const Home = ({ myList, trends, originals }) => {
   return (
