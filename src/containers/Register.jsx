3f39021cdbdf71e8553390d8de1b66a53f767a72
Jose-Rivera <joseriveradl@gmail.com>
2020-10-07 19:30:24 -0600

    working register container

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,23 +1,68 @@
 
-import '../assets/styles/components/Login.scss';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import '../assets/styles/components/Login.scss';
 
-const Register = () => (
-  <section className="login">
-    <section className="login__container">
-      <h2>Regístrate</h2>
-      <form action="" className="login__container--form">
-        <input className="input" type="text" placeholder="Nombre" />
-        <input className="input" type="text" placeholder="Correo" />
-        <input className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Registrarme</button>
-      </form>
-      <div className="login__container--inicio">
-        <Link to="/login">Iniciar sesión</Link>
-      </div>
+const Register = props => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
+    })
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  }
+
+  return (
+    <section className="login">
+      <section className="login__container">
+        <h2>Regístrate</h2>
+        <form action="" className="login__container--form" onSubmit={handleSubmit}>
+          <input
+            name="name"
+            className="input"
+            type="text"
+            placeholder="Nombre"
+            onChange={handleInput}
+          />
+          <input
+            name="email"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button">Registrarme</button>
+        </form>
+        <div className="login__container--inicio">
+          <Link to="/login">Iniciar sesión</Link>
+        </div>
+      </section>
     </section>
-  </section>
-);
+  );
+}
 
-export default Register;
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
